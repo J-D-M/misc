5524849616d98ed4bdd350b7bbaf14a9034c12f3
eyoson <eyoson@waifu.club>
2018-04-23 13:53:28 -0700

    started hash

--- structs/hashtables/chainedHash.hpp
+++ structs/hashtables/chainedHash.hpp
@@ -1,37 +1,89 @@
+#include <algorithm>
+#include <cmath>
+#include <initializer_list>
 #include <list>
 #include <string>
+#include <utility>
 #include <vector>
 
 template <typename Key, typename Elem> class ChainedHash
 {
       private:
-	std::vector<std::list<Elem>> data;
-	size_t size;
+	std::vector<std::list<std::pair<Key, Elem>>> data; // table
+	size_t elements;
+	size_t capacity;
+	std::pair<Key, Elem> default_pair;
+
+	// primality check for resize()
+	auto isPrime(size_t x) const -> bool
+	{
+		size_t max = sqrt(x);
+		for (size_t i = 2; i < max; i++)
+			if (x % i == 0)
+				return false;
+		return true;
+	}
+
+	// get new size of table
+	auto resize() const -> void
+	{
+		// find next prime
+		if (capacity == 0) {
+			capacity = 7;
+		} else {
+			capacity *= 2;
+			while (!isPrime(capacity))
+				capacity++;
+		}
+
+		// temp copy
+		auto tmp = data;
+		data.clear();
+		data.resize(capacity);
+		// push everything back in with new hash
+		for (auto const &ls : data)
+			for (auto const &elem : ls)
+				insert(elem);
+	}
 
 	auto hash(Key const &k) -> size_t;
 
       public:
-	ChainedHash() {}
-	~ChainedHash() {}
-
+	// insert elem
 	auto insert(Key const &k, Elem const &e) const -> void
 	{
-		if (size > data.size * 0.7)
-			data.resize(data.size * 2);
+		auto ls    = data[hash(k)];
+		auto itter = std::find(ls.begin(), ls.end(), k);
 
-		data[find(k)] = e;
+		if (itter != ls.end())
+			*itter = e;
+		else
+			ls.push_back(e);
+
+		elements++;
 	}
 
-	auto find(Key &) const -> Elem &;
-	auto del(Key &) const -> void;
+	// delete element
+	auto del(Key const &k) const -> void
+	{
+		auto ls    = data[hash(k)];
+		auto itter = std::find(ls.begin(), ls.end(), k);
 
-	auto operator[](Key const &k) const -> Elem & { return find(k); }
+		if (itter != ls.end()) {
+			ls.erase(itter);
+			elements--;
+		}
+	}
 
+	auto size() const -> size_t { return elements; }
+
+	ChainedHash() {}
+
+	ChainedHash(std::initializer_list<std::pair<Key, Elem>> ls)
+	{
+		for (auto const &l : ls)
+			insert(l.first, l.second);
+	}
+
+	~ChainedHash() {}
 };
-
-template <typename Elem>
-auto ChainedHash<std::string, Elem>::hash(std::string const &str) -> size_t {
-	return 1;
-}
-
-
