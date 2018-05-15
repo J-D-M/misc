f0b1e43ef10b9c551f6636828390970bce818281
eyoson <eyoson@waifu.club>
2018-05-15 09:29:13 -0700

    queue c

--- structs/queue.c
+++ structs/queue.c
@@ -6,6 +6,7 @@
 
 struct Queue {
 	void **data_;
+	size_t data_size_;
 	size_t capacity_;
 	size_t size_;
 	size_t head_;
@@ -25,11 +26,38 @@ queueNew(size_t d)
 {
 	struct Queue *ret_q = malloc(sizeof(struct Queue));
 
-	ret_q->data_     = NULL;
-	ret_q->capacity_ = 0;
-	ret_q->size_     = 0;
-	ret_q->head_     = 0;
-	ret_q->tail_     = 0;
+	ret_q->data_      = NULL;
+	ret_q->data_size_ = d;
+	ret_q->capacity_  = 0;
+	ret_q->size_      = 0;
+	ret_q->head_      = 0;
+	ret_q->tail_      = 0;
 
 	return ret_q;
+}
+
+/*
+ * Function: Queue_resize_
+ * double size of void **data_ or allocate space for a single value if empty
+ * abort() called on failure
+ * ------------------------------
+ * @param q: pointer to queue
+ */
+
+void
+Queue_resize_(struct Queue *q)
+{
+	if (q->capacity_ == 0) {
+		q->capacity_ = 1;
+		q->data_     = malloc(sizeof(void *));
+	} else {
+		q->capacity_ *= 2;
+
+		void **tmp = q->data_;
+		q->data_   = malloc(sizeof(void *) * q->capacity_);
+
+		if (q->head_ > q->tail_) {
+			memcpy(q->data_, tmp + q->head_, q->)
+		}
+	}
 }
---
+++ structs/queue.h
@@ -0,0 +1,7 @@
+#ifndef _JDM_QUEUE_
+#define _JDM_QUEUE_
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#endif
